df30c354fe804fbb56aff97e4d51021038f86c57
arvidbt <dv20arn@cs.umu.se>
2022-07-26 13:34:06 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
+<!-- eslint-disable no-unused-vars -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Is it a word?</h1>
+  <!-- <p>Message is: {{ message }}</p> -->
+
+  <input v-model="message" placeholder="Enter word.." />
+  <button @click="getWords()">Run the search!</button>
+  <div v-if="this.isAWord">
+    <h1>yay! that is a word!</h1>
+
+  </div>
+  <div v-else>
+    <h1>Nay! That's not a word!</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  mounted() {
+    this.loadSAOLDictionary();
+  },
+  data() {
+    return {
+      message: '',
+      eligableWords: null,
+      maxWordLength: 0,
+      isAWord: false,
+    }
+  },
+  methods:  {
+    calculateWordPoints() {
+
+    },
+    wordStartsWith() {
+
+    },
+    wordEndsWith() {
+
+    },
+
+    getWordsContainingLetters() {
+      let filtered_words = this.eligableWords;
+      for (const i in this.message) {
+        filtered_words = filtered_words.filter(word => word.includes(this.message[i]));
+      }
+      for (const i in filtered_words) {
+        let remove_index = 0;
+        for (const j in filtered_words[i]) {
+          // console.log(filtered_words[i][j])
+          if(!filtered_words[i].includes(filtered_words[i][j])) {
+            remove_index = i;
+            console.log(remove_index)
+          }
+        }
+        filtered_words.splice(remove_index, 1)
+      }
+
+      console.log(filtered_words);
+    },
+
+    checkIfWord() {
+      this.isAWord = this.eligableWords.includes(this.message);
+    },
+
+    async loadSAOLDictionary() {
+      this.eligableWords = await require('../wordlist.json');      
+    },
+    getMaxWordLength() {
+      return this.message.length;
+    },
+    getWords() {
+      if(!this.message) {
+        return;
+      }
+      this.maxWordLength = this.getMaxWordLength();
+
+      this.eligableWords = this.eligableWords.filter(word => word.length <= this.maxWordLength);
+      this.checkIfWord();
+      this.getWordsContainingLetters();
+      this.eligableWords = this.loadSAOLDictionary();
+
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

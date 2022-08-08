a3192b3a5747847d9f4d485b62904019f9583844
arvidbt <dv20arn@cs.umu.se>
2022-08-08 22:43:41 +0200

    deploy bug? x3

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,20 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/wordfeusk/" : "/",
 };
 
-// module.exports = {
-//   pwa: {
-//     name: "WordFeusk",
-//     themeColor: "#B9DEFE"
-//   }
-// };
+module.exports = {
+  pwa: {
+    name: 'BDFI',
+    themeColor: '#2d3748',
+    msTileColor: '#2d3748',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestPath: 'manifest.json',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'service-worker.js',
+        // ...other Workbox options...
+        exclude: [/_redirects/],
+    }
+}
+};

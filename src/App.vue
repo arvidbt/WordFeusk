afe62718f7d0c58ab3822df827df9f9e21621952
arvidbt <dv20arn@cs.umu.se>
2022-08-09 17:35:31 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <!-- eslint-disable no-unused-vars -->
 <template>
   <div class="wrapper">
-
     <div class="title-container">
       <h1 class="title-text">WordFeusk</h1>
       <p class="help-text" style="margin-left: 5px; color: #F1C40F;"> BETA</p>
       <svg class="search-button" @click="toggleAdvancedMode()" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#fff" stroke-width="2" d="M15,16 L21,22 L15,16 Z M10,18 C13.8659932,18 17,14.8659932 17,11 C17,7.13400675 13.8659932,4 10,4 C6.13400675,4 3,7.13400675 3,11 C3,14.8659932 6.13400675,18 10,18 Z M20,1 L20,7 M17,4 L23,4"/></svg>
-      <svg class="search-button" @click="toggleHelp()" style="margin-right: 60px;" width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="helpIconTitle" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" color="#000000"> <title id="helpIconTitle">Help</title> <path d="M12 14C12 12 13.576002 11.6652983 14.1186858 11.1239516 14.663127 10.5808518 15 9.82976635 15 9 15 7.34314575 13.6568542 6 12 6 11.1040834 6 10.2998929 6.39272604 9.75018919 7.01541737 9.49601109 7.30334431 9.29624369 7.64043912 9.16697781 8.01061095"/> <line x1="12" y1="17" x2="12" y2="17"/> <circle cx="12" cy="12" r="10"/> </svg>
+      <svg class="search-button" @click="toggleHelp()" style="margin-right: 64px;" width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="helpIconTitle" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" color="#000000"> <title id="helpIconTitle">Help</title> <path d="M12 14C12 12 13.576002 11.6652983 14.1186858 11.1239516 14.663127 10.5808518 15 9.82976635 15 9 15 7.34314575 13.6568542 6 12 6 11.1040834 6 10.2998929 6.39272604 9.75018919 7.01541737 9.49601109 7.30334431 9.29624369 7.64043912 9.16697781 8.01061095"/> <line x1="12" y1="17" x2="12" y2="17"/> <circle cx="12" cy="12" r="10"/> </svg>
     </div>
     <div v-if="this.showHelp" class="footer-container" style="margin-top: -10px;">
-      <p class="help-text" style="font-size: 12px;">
-        <span style="color: #F1C40F">-</span> STRIKT ORDLISTA<br>
-        <span style="font-size: 10px;color: #B9DEFE; ">&nbsp;&nbsp;BÖJDA ORD KOMMER INTE MED I RESULTATET.<br>
-        &nbsp;&nbsp;BOKSTÄVERNA <span style="color: #F1C40F">UFASKT</span> GER <span style="color: #F1C40F">FUSKA</span>, MEN INTE <span style="color: #F1C40F">FUSKAT</span>.<br><br></span>
-        <span style="color: #F1C40F;">-</span> BLANKA BRICKOR<span style="color: #F1C40F"></span><br>
-        <span style="font-size: 10px;color: #B9DEFE; ">&nbsp;&nbsp;FÖR ATT ANVÄNDA BLANKA RUTOR SKRIV IN <span style="color: #F1C40F">?</span> I SÖKRUTAN.</span>
-        </p><br>
+      <info-display/>
     </div>
     <div class="footer-container">
       <input autocomplete="off" v-show="this.showAdvanced" type="text" id="begin-input" class="adv-search-box" style="margin-left: 15px;" v-model="beginsWith" placeholder="BÖRJAR MED"/>
@@ -61,217 +54,20 @@
       </div>
     </div>
 </template>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
-
-p, h1, h3 {
-  font-family: 'Roboto', sans-serif;
-}
-
-html, body {
-  background-color: #B9DEFE;
-}
-
-*:focus {
-  outline: none;
-}
-::placeholder {
-  font-style: italic;
-  font-size: 10px;
-  color: #B9DEFE;
-  font-style: italic;
-  font-weight: bold;
-  font-family: 'Roboto', sans-serif;
-}
-.search-box {
-  background-color: #199EF3;
-  font-size: 18px;
-  width: 100%;
-  font-weight: bold;
-  color: #fff;
-  border-radius: 7.5px;
-  height: 34px;
-  border: none;
-  margin: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 15px;
-  padding-bottom: 15px;
-  padding-left: 5px;
-  padding-right: 5px;
-  text-align: center;
-  text-transform:uppercase;
-  font-family: 'Roboto', sans-serif;
-}
-
-.adv-search-box {
-  background-color: #199EF3;
-  font-size: 16px;
-  width: 60%;
-  font-weight: bold;
-  color: #F1C40F;
-  border-radius: 7.5px;
-  height: 34px;
-  border: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 15px;
-  padding-bottom: 15px;
-  padding-left: 5px;
-  padding-right: 5px;
-  text-align: center;
-  text-transform:uppercase;
-  font-family: 'Roboto', sans-serif;
-}
-
-.title-container {
-  width: 100%;
-  background-color: #0082F0;
-  display: flex;
-  align-items: center;
-  border-radius: 7.5px;
-}
-
-.footer-container {
-  width: 100%;
-  background-color: #0082F0;
-  display: flex;
-  align-items: center;
-  border-radius: 7.5px;
-  margin-top: 10px;
-  /* position: fixed; */
-  transition: 0.3s ease-in-out;
-}
-
-.background-container {
-  margin-top: 10px;
-  overflow-y: auto;
-  background: #0082F0;
-  /* height: 67vh; */
-  flex-direction: column;
-  max-height: 95vh;
-  display: flex;
-  flex: 1;
-  padding: 10px;
-  border-radius: 7.5px;
-  margin-bottom: -15px;
-}
-
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  max-height: 95vh;
-  min-height: 95vh;
-}
-
-.title-text {
-  color: #fff;
-  font-size: 28px;
-  font-weight: bold;
-  margin-left: 15px;
-}
-
-.title-list {
-  color: #fff;
-  font-size: 36px;
-  font-weight: bold;
-}
-
-.word-text {
-  color: #fff;
-  font-weight: bold;
-  font-size: 18px;
-}
-
-.help-text {
-  color: #fff;
-  font-size: 14px;
-  font-style: italic;
-  font-weight: bold;
-  margin-left: 15px;
-}
-
-.list-titles {
-  color: #fff;
-  font-size: 16px;
-  font-style: italic;
-  font-weight: bold;
-}
-
-.search-button {
-  width: 34px;
-  padding: 10px;
-  background-color: #199EF3;
-  border-radius: 5px;
-  border: none;
-  color: #fff;
-  /* color: #F1C40F; */
-  font-size: 14px;
-  font-style: italic;
-  font-weight: bold;
-  margin-left: 15px;
-  font-family: 'Roboto', sans-serif;
-  position: absolute;
-  right: 25px;
-}
-
-
-.list-container {
-  max-width: 100%;
-  height: fit-content;
-  background-color: #199EF3;
-  /* display: ; */
-  display: flex;
-  flex-wrap: wrap;
-  /* overflow: auto; */
-  margin-bottom: 10px;
-  border-radius: 5px;
-  padding: 10px;
-}
-
-.word-container {
-  width: fit-content;
-  padding: 10px;
-  background-color: #0082F0;
-  margin: 5px;
-  border-radius: 5px;
-  /* border: 5px solid #199EF3;
-  box-shadow: 5px 5px #0082F0; */
-}
-
-.word-container:hover {
-  cursor: pointer;
-}
-
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-::-webkit-scrollbar-track {
-  background: #0082F0;
-  border-radius: 7.5px;
-}
-
-::-webkit-scrollbar-thumb {
-  background: #199EF3;
-}
-
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
+<style src="./assets/styles.css">
 
 </style>
 
 
 <script>
 import WordBox from "@/components/WordBox.vue";
+import InfoDisplay from "@/components/InfoDisplay.vue"
 
 export default {
   name: 'App',
   components: {
     WordBox,
+    InfoDisplay,
   },
 
   mounted() {
@@ -310,6 +106,13 @@ export default {
     }
   },
   methods:  {
+    redirectGithub() {
+      window.open('https://github.com/arvidbt/wordfeusk', '_blank').focus();
+    },
+
+    redirectLinkedIn() {
+      window.open('https://www.linkedin.com/in/arvid-bergman-th%C3%B6rn-1843701b8/', '_blank').focus();
+    },
     toggleAdvancedMode() {
       this.showAdvanced = !this.showAdvanced;
       this.added = 1;
---
+++ src/components/InfoDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+<p class="help-text" style="font-size: 12px;">
+        <span style="color: #F1C40F">-</span> STRIKT ORDLISTA<br>
+        <span style="font-size: 10px;color: #B9DEFE;">
+          &nbsp;&nbsp;BÖJDA ORD KOMMER INTE MED I RESULTATET.<br>
+          &nbsp;&nbsp;BOKSTÄVERNA <span style="color: #F1C40F">UFASKT</span> GER <span style="color: #F1C40F">FUSKA</span>, MEN INTE <span style="color: #F1C40F">FUSKAT</span>.<br><br>
+        </span>
+        <span style="color: #F1C40F;">-</span> BLANKA BRICKOR<br>
+        <span style="font-size: 10px;color: #B9DEFE;">
+          &nbsp;&nbsp;FÖR ATT ANVÄNDA BLANKA RUTOR SKRIV IN <span style="color: #F1C40F">?</span> I SÖKRUTAN.</span><br><br>
+        <span style="color: #F1C40F">-</span> SKAPAD AV ARVID BERGMAN.<br>
+        <span style="font-size: 10px;color: #B9DEFE;">
+          &nbsp;&nbsp;FÖR ATT SE KÄLLKOD, SE <span style="color: #F1C40F" @click="this.redirectGithub()">GITHUB</span>.<br>
+          &nbsp;&nbsp;FÖR KONTAKT, SE <span style="color: #F1C40F" @click="this.redirectLinkedIn()">LINKEDIN</span>.</span><br><br>
+      </p>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'InfoDisplay',
+  methods: {
+    redirectGithub() {
+      window.open('https://github.com/arvidbt/wordfeusk', '_blank').focus();
+    },
+    redirectLinkedIn() {
+      window.open('https://www.linkedin.com/in/arvid-bergman-th%C3%B6rn-1843701b8/', '_blank').focus();
+    },
+  }
+}
+</script>

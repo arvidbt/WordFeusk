5636d6ad15d6b011c558a92386c6a4a50eacc087
arvidbt <dv20arn@cs.umu.se>
2022-08-09 15:40:12 +0200

    automatic script deployment

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
     <div class="title-container">
       <h1 class="title-text">WordFeusk</h1>
       <p class="help-text" style="margin-left: 5px; color: #F1C40F;"> BETA</p>
-      <!-- <button class="search-button" @click="toggleHelp()">INFO</button> -->
-      <!-- <button style="margin-right: 60px;" class="search-button" @click="toggleAdvancedMode()">A.SÖK</button> -->
       <svg class="search-button" @click="toggleAdvancedMode()" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#fff" stroke-width="2" d="M15,16 L21,22 L15,16 Z M10,18 C13.8659932,18 17,14.8659932 17,11 C17,7.13400675 13.8659932,4 10,4 C6.13400675,4 3,7.13400675 3,11 C3,14.8659932 6.13400675,18 10,18 Z M20,1 L20,7 M17,4 L23,4"/></svg>
       <svg class="search-button" @click="toggleHelp()" style="margin-right: 60px;" width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="helpIconTitle" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" color="#000000"> <title id="helpIconTitle">Help</title> <path d="M12 14C12 12 13.576002 11.6652983 14.1186858 11.1239516 14.663127 10.5808518 15 9.82976635 15 9 15 7.34314575 13.6568542 6 12 6 11.1040834 6 10.2998929 6.39272604 9.75018919 7.01541737 9.49601109 7.30334431 9.29624369 7.64043912 9.16697781 8.01061095"/> <line x1="12" y1="17" x2="12" y2="17"/> <circle cx="12" cy="12" r="10"/> </svg>
     </div>
--- vue.config.js
+++ vue.config.js
@@ -10,16 +10,5 @@ module.exports = {
 module.exports = {
   pwa: {
     name: 'WordFeusk',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    // configure the workbox plugin
-    // workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      // swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 };
